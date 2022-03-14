bab4a396486c8244e7d1f2bd8fdfb9d87c1d4d66
Hind3ight <1157595693@qq.com>
2022-03-14 09:03:41 +0800

    修改Lissajous程序，修改其调色板来生成更丰富的颜色，然后修改SetColorIndex的第三个参数

--- ch1/lissajous/main.go
+++ ch1/lissajous/main.go
@@ -27,12 +27,13 @@ import (
 )
 
 //!+main
-
-var palette = []color.Color{color.White, color.Black}
+// 定义新的调色板
+var palette = []color.Color{color.RGBA{255, 255, 255, math.MaxUint8}, color.Black, color.RGBA{0, 0, 128, math.MaxUint8}, color.RGBA{30, 144, 255, math.MaxUint8}, color.RGBA{72, 61, 139, math.MaxUint8}, color.RGBA{255, 0, 255, math.MaxUint8}, color.RGBA{124, 252, 0, math.MaxUint8}}
 
 const (
 	whiteIndex = 0 // first color in palette
 	blackIndex = 1 // next color in palette
+	greenIndex = 2 // next color in palette 定义新的颜色
 )
 
 func main() {
@@ -67,14 +68,18 @@ func lissajous(out io.Writer) {
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0 // phase difference
-	for i := 0; i < nframes; i++ {
+	for i, c := 0, 1; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				uint8(c)) //使用新的颜色
+		}
+		c++ //修改颜色选择
+		if c >= len(palette) {
+			c = 1
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
